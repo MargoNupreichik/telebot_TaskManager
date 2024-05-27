eecfb978650c80735dc6ccd4c236856f08cd8029
arlenochka <m.nupreichik@yandex.ru>
2024-05-27 23:09:53 +0300

    adding task pipeline is ready

--- main.py
+++ main.py
@@ -51,7 +51,36 @@ def date_step1(message):
         bot.register_next_step_handler(message, date_step1)
 
 def title_step2(message):
-    pass
+    if message.content_type == 'text':
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = telebot.types.KeyboardButton("Да")
+        btn2 = telebot.types.KeyboardButton("Нет")
+        markup.add(btn1, btn2)
+        bot.send_message(message.chat.id, posts.new_task_text_instruction3, reply_markup=markup)
+        bot.register_next_step_handler(message, notes_step3)             
+    else:
+        bot.send_message(message.chat.id, "Введите текст.")
+        bot.register_next_step_handler(message, title_step2)     
 
+def notes_step3(message):
+    if message.text == "Да":
+        bot.send_message(message.chat.id, posts.new_task_text_instruction4)
+        bot.register_next_step_handler(message, media_step4)             
+    elif message.text == "Нет":
+        bot.register_next_step_handler(message, saving_step5)  
+    else:
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = telebot.types.KeyboardButton("Да")
+        btn2 = telebot.types.KeyboardButton("Нет")
+        markup.add(btn1, btn2)
+        bot.send_message(message.chat.id, "Команда не распознана. Воспользуйтесь кнопками.", reply_markup=markup)
+         
+def media_step4(message):
+    bot.send_message(message.chat.id, posts.new_task_text_instruction5)
+    bot.register_next_step_handler(message, saving_step5)        
+
+def saving_step5(message):
+    bot.send_message(message.chat.id, posts.new_task_text_instruction6)
+         
 # bot launching
 bot.polling(none_stop=True, interval=0)
